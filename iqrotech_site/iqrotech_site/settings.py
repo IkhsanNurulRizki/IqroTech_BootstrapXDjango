d4ea4af03d8d79524cacc177d639e3f5127d2ebc
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2023-12-06 21:56:01 +0700

    adding host

--- iqrotech_site/iqrotech_site/settings.py
+++ iqrotech_site/iqrotech_site/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,6 +114,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
