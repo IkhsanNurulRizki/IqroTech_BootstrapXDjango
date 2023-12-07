11d279d4bf0f29e08e0e060db24bf6f5355f1847
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2023-12-07 10:00:32 +0700

    update

--- iqrotech_site/iqrotech_site/settings.py
+++ iqrotech_site/iqrotech_site/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-i48cq*k!d6+sys!-^0xvz9na_k5e026t#k1_^3wdq9^0%4+ye1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.iqrotech.test', 'iqrotech.test', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
