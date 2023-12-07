0644eac3fc9bef3161cff86c1322f33d1a002605
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2023-12-07 16:16:38 +0700

    update

--- iqrotech_site/iqrotech_site/urls.py
+++ iqrotech_site/iqrotech_site/urls.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-from django.conf.urls import url
-from django.views.static import serve
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
@@ -14,7 +11,4 @@ urlpatterns = [
     path('tentang/', include('tentang.urls')),
     path('testimoni/', include('testimoni.urls')),
     path('me/', include('me.urls')),
-
-    url(r'^media/(?<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
