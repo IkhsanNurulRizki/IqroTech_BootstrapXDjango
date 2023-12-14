955a989994190028dd80f49eb3702e3e02665fbd
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2023-12-14 16:58:21 +0700

    update

--- iqrotech_site/iqrotech_site/settings.py
+++ iqrotech_site/iqrotech_site/settings.py
@@ -123,6 +123,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
