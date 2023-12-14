86408fe56b733df72e6cc3e9f4d03a4d4da3712b
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2023-12-14 17:20:08 +0700

    update

--- iqrotech_site/iqrotech_site/settings.py
+++ iqrotech_site/iqrotech_site/settings.py
@@ -121,9 +121,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
